8fce4d2b112aa253506aa619bce3eda16cb579f0
kelesmert <mertkelesed@gmail.com>
2024-08-14 22:21:50 +0300

    anjana quiz commit

---
+++ denemeler/den2.js
@@ -0,0 +1,13 @@
+const fact = {
+
+    "statement" : "adas",
+    true :"this is true",
+    false : "this is false"
+
+}
+
+console.log(typeof(fact.true))
+
+console.log(fact.true)
+
+console.log(typeof(Object.keys(fact)[1]))
